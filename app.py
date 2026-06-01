01cdbef68893d0639d49d4b781adab7de69bb63e
Deon <gideontrimanharefa12@gmail.com>
2026-06-01 16:29:24 +0700

    Membuat tampilannya agar lebih baik

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def transaksi():
 
 @app.route('/logout')
 def logout():
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
